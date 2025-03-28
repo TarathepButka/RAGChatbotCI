46442a94852ea1a4955a0e9aa14e7f6268606a2e
Putthipong21 <putthipong.ki@kkumail.com>
2025-03-28 16:24:57 +0700

    add home page and silly library

--- ragchatbotci/src/app/page.tsx
+++ ragchatbotci/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
 import Navbar from "@/components/Navbar";
 import AboutUsPage from "@/components/About";
@@ -16,21 +17,25 @@ export default function HomePage() {
     <>
       <Navbar />
       <Container className="mt-24">
-        <div id="home" className="flex items-center w-full lg:w-1/2">
-          <div className="max-w-2xl">
+        <div
+          id="home"
+          className="flex items-center justify-center w-full lg:w-1/2"
+        >
+          <div className="max-w-2xl lg:ml-16">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-white lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight">
               Welcome to RAGKKUCHAT
             </h1>
-            <p className="py-5 text-xl leading-normal text-gray-500 lg:text-lg xl:text-xl dark:text-gray-300">
+            <p className="py-5 text-base leading-normal text-gray-500 lg:text-lg xl:text-xl dark:text-gray-300">
               Easily to know about KKU.
             </p>
-            <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
-              <a
-                className="px-8 py-4 text-lg font-medium text-center text-white bg-blue-700 hover:bg-blue-800 rounded-md cursor-pointer"
+            <div className="flex md:order-2 space-x-0 rtl:space-x-reverse">
+              <button
+                type="button"
+                className="text-white text-xl bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text- px-6 py-3 text-center cursor-pointer"
                 onClick={() => router.push("/chat")}
               >
-                Start now
-              </a>
+                Let's Chat
+              </button>
             </div>
           </div>
         </div>
@@ -46,10 +51,71 @@ export default function HomePage() {
         </div>
       </Container>
       <Container>
+        <div className="w-full flex justify-center">
+          <h1 className="text-center w-full text-2xl font-semibold leading-snug tracking-tight lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight bg-gradient-to-r from-[#f34a62] via-[#bb77ed] to-[#1cdce8] inline-block text-transparent bg-clip-text">
+            AI Assistant for Engineering <br />
+            at Khon Kaen University
+          </h1>
+        </div>
+        <div className="flex items-center justify-center w-full mt-8 lg:w-1/2">
+          <DotLottieReact
+            className="object-cover"
+            src="https://lottie.host/2a04e9ed-074e-4695-9f24-37f58492d256/FZCgJ98JFY.lottie"
+            width="616"
+            height="617"
+            loop
+            autoplay
+          />
+        </div>
+        <div className="flex items-center justify-center w-full mt-8 lg:w-1/2">
+          <section className="text-sm font-normal leading-snug tracking-tight text-white lg:text-base lg:leading-tight xl:text-lg xl:leading-tight">
+            Our{" "}
+            <span className="bg-gradient-to-r from-[#f9655b] to-[#ee821a] inline-block text-transparent font-bold bg-clip-text">
+              AI-powered chatbot
+            </span>{" "}
+            is designed to help students and faculty with academic-related
+            inquiries, including:
+          </section>
+        </div>
+      </Container>
+      <Container>
+        <div className="mx-16 mb-4 items-center p-2 rounded-4xl border border-gray-600">
+          <p className="text-white text-lg mx-4">About</p>
+        </div>
+        <div className="flex justify-between gap-6 mx-16">
+          <div className="max-w-2xl">
+            <h1 className="text-xl font-semibold leading-snug tracking-tight text-white lg:text-2xl lg:leading-tight xl:text-4xl xl:leading-tight">
+              A Project in Computational Intelligence <br />
+              by Prof. Wanida
+            </h1>
+          </div>
+          <div className="max-w-2xl ">
+            <h1 className="text-sm font-normal leading-snug tracking-tight text-white lg:text-base lg:leading-tight xl:text-lg xl:leading-tight mt-2 ">
+              This chatbot is specifically designed to assist students, <br />
+              faculty, and researchers in navigating academic inquiries <br />
+              related to Engineering at{" "}
+              <span className="bg-gradient-to-r from-[#f9655b] to-[#ee821a] inline-block text-transparent bg-clip-text font-bold">
+                Khon Kaen University
+              </span>
+              .
+            </h1>
+          </div>
+        </div>
+        <div className="relative w-[1024px] h-[576px] mx-auto mb-4 my-6">
+          <Image
+            src={"/images/en.jpg"}
+            alt={`en`}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-2xl"
+          />
+        </div>
+      </Container>
+      <Container>
         <AboutUsPage />
       </Container>
-      <Footer />
       <ChatButton />
+      <Footer />
     </>
   );
 }
--- ragchatbotci/src/components/Navbar.tsx
+++ ragchatbotci/src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import NextLink from "next/link";
+import { useRouter } from "next/navigation";
 import { Link as ScrollLink } from "react-scroll";
 
 export default function Navbar() {
@@ -9,6 +10,8 @@ export default function Navbar() {
     { name: "Home", href: "/" },
     { name: "About Us", href: "about" },
   ];
+
+  const router = useRouter();
 
   return (
     <nav className="bg-[#1b1b20] fixed w-full z-20 top-0 start-0 border-b border-gray-600 py-4">
@@ -18,28 +21,49 @@ export default function Navbar() {
             <span>RAGKKUCHAT</span>
           </span>
         </NextLink>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text- px-4 py-2 text-center cursor-pointer">Get started</button>
-          <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-400 rounded-lg md:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-            <span className="sr-only">Open main menu</span>
-              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-            </svg>
+        <div className="flex md:order-2 space-x-0 rtl:space-x-reverse">
+          <button
+            type="button"
+            className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text- px-4 py-2 text-center cursor-pointer"
+            onClick={() => router.push("/chat")}
+          >
+            Let's Chat
           </button>
         </div>
-        <div className="text-lg items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+        <div className="text-lg items-center justify-between w-full md:flex md:w-auto md:order-1 hidden lg:flex">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg bg-[#1b1b20] md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
             <li>
-              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+              <a
+                href="#"
+                className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                aria-current="page"
+              >
+                Home
+              </a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Services
+              </a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                About
+              </a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Our Team</a>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Our Team
+              </a>
             </li>
           </ul>
         </div>
